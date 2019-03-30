fa1747391c81a4d685c1a0f8e089a1701fd7d58b
Jean-vincent GUGLIELMETTI <jeguglie@e1r11p4.42.fr>
2019-03-31 00:08:52 +0100

    fuck

--- admin/admin.php
+++ admin/admin.php
@@ -1,13 +1,19 @@
+<html>
+    <head>
+        <link rel="stylesheet" href="../css/style_admin.css" />
+        <link href="http://fonts.googleapis.com/css?family=Terminal+Dosis" rel="stylesheet" type="text/css" />
+    </head>
+</html>
 <?php
 session_start();
-if(isset($_SESSION['username']) AND !empty($_SESSION['username'])) 
+if(isset($_SESSION['login']) AND !empty($_SESSION['login'])) 
 {
     if(isset($_GET['action']))
     { 
         if($_GET['action'] == 'addproduct')
         {
-            if(isset($_POST['submit'])){
-                
+            if(isset($_POST['submit']))
+            {  
                 $title = $_POST['title']; // 255 char
                 $description=$_POST['description']; //500 char
                 $price=$_POST['price']; // only digit et only positif
@@ -17,20 +23,20 @@ if(isset($_SESSION['username']) AND !empty($_SESSION['username']))
                 if (!($title&&$description&&$price&&$quantite&&$ID)){
                     echo "Veuillez remplir tous les champs\n";
                 }
-                if (strlen($title) >= 255)
-                echo "Titre supérieur à 255 char\n";
+                if (strlen($title) >= 50)
+                    echo "Titre supérieur à 50 char\n";
                 elseif (strlen($description) >= 500)
-                echo "Description supérieur à 500 char\n";
+                    echo "Description supérieur à 500 char\n";
                 elseif ($price < 0)
-                echo "Tu veux vraiment lui donner du biff?\n";
+                    echo "Tu veux vraiment lui donner du biff?\n";
                 elseif ($quantite <= 0)
-                echo "Quantité strictement positive\n";
+                    echo "Quantité strictement positive\n";
             }
             ?>
                 <form action ="" method="post">
                 <h3>Titre du produit:</h3><input type="text" name="title"/>
                 <h3>Description du produit:</h3><input type="text" name="description"/>
-                <h3>Quantité:</h3><input type="text" name="qt"/>
+                <h3>Quantité:</h3><input type="number" name="qt"/>
                 <h3>Prix:</h3><input type="number" name="price"/><br/><br/>
                 <h3>Image:</h3><input type="text" name="img"/>
                 <input type="submit" name="submit"/>
@@ -56,12 +62,6 @@ if(isset($_SESSION['username']) AND !empty($_SESSION['username']))
                     echo "Pseudo supérieur à 20 char\n";
                 elseif (strlen($adresse) >= 500)
                     echo "Description supérieur à 500 char\n";
-                elseif (!is_numeric($price))
-                    echo "Le prix n'est pas un digit\n";
-                elseif ($price < 0)
-                    echo "Tu veux vraiment lui donner du biff?\n";
-                elseif ($quantite <= 0)
-                    echo "Quantité strictement positive\n";
             }
 ?>
             <form action ="" method="post">
@@ -75,11 +75,8 @@ if(isset($_SESSION['username']) AND !empty($_SESSION['username']))
 
             <?php
         }
-        elseif ($_GET['action'] == 'addcat')
-        {
-            if (isset($_POST['submit']))
-            {
-    else if($_GET['action'] == 'modify'){
+    else if($_GET['action'] == 'modify')
+    {
 
     }
     else if($_GET['action'] == 'delete'){
@@ -97,16 +94,21 @@ else{
 }
 ?>
 
-<h1>Bienvenue, <?php $_SESSION['username']; ?><h1>
-<br/>
-<a href="?action=addproduct">Ajouter un produit</a>
-<a href="?action=modify">Modifier un produit</a>
-<a href="?action=delete">Supprimer un produit</a>
-<br>
-<a href="?action=adduser">Ajouter un utilisateur</a>
-<a href="?action=modify">Modifier un utilisateur</a>
-<a href="?action=delete">Supprimer un utilisateur</a>
-<br>
-<a href="?action=addcat">Ajouter un catégorie</a>
-<a href="?action=modify">Modifier un catégorie</a>
-<a href="?action=delete">Supprimer un catégorie</a>
+<div class="admin_container">
+    <h1 id="title"> Bienvenue <h1>
+    <ul>
+        <li><a href="?action=addproduct">Ajouter un produit</a></li>
+         <li><a href="?action=modify">Modifier un produit</a></li>
+        <li><a href="?action=delete">Supprimer un produit</a></li>
+    </ul>
+     <ul>
+        <li><a href="?action=adduser">Ajouter un utilisateur</a></li>
+        <li><a href="?action=modify">Modifier un utilisateur</a></li>
+         <li><a href="?action=delete">Supprimer un utilisateur</a></li>
+        </ul>
+     <ul>
+        <li><a href="?action=addcat">Ajouter un catégorie</a></li>
+        <li><a href="?action=modify">Modifier un catégorie</a></li>
+        <li><a href="?action=delete">Supprimer un catégorie</a></li>
+    </ul>
+</div>
