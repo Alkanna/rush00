a0e6119fa2282a46ffdc480202f0e66ceb11040b
Khiman 'klouer' Louer <alkcreas@gmail.com>
2019-03-31 01:35:26 +0100

    Seems we can't get data for some reason, trying something else

--- connection_testing.php
+++ connection_testing.php
@@ -3,19 +3,19 @@
 include 'sqlib.php';
 
 if (connect_mysql_serveur()) {
-    echo "Server connect : Success !\n";
+    echo "[Server connect : Success !]\n";
 } else {
-    echo "Server connect :Failure !\n";
+    echo "[Server connect :Failure !]\n";
 }
 
 if ($con = connect_db()) {
-    echo "DB Connect : Success !\n";
+    echo "[DB Connect : Success !]\n";
 } else {
-    echo "DB Connect : Failure !\n";
+    echo "[DB Connect : Failure !]\n";
 }
 
-if (is_pseudo_exist($con, "root")) {
-    echo "We can read data !";
+if (is_pseudo_exist($con, 'root')) {
+    echo "[We can read data !]";
 } else {
-    echo "We can't read data :(";
+    echo "[We can't read data :(]";
 }
