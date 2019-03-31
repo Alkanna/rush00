d8dc1c153e7d5331c75605584e757e251d91f63d
Khiman 'klouer' Louer <alkcreas@gmail.com>
2019-03-31 01:31:06 +0100

    Added some testing for sql

--- connection_testing.php
+++ connection_testing.php
@@ -3,7 +3,19 @@
 include 'sqlib.php';
 
 if (connect_mysql_serveur()) {
-    echo "Success !";
+    echo "Server connect : Success !\n";
 } else {
-    echo "Failure !";
+    echo "Server connect :Failure !\n";
+}
+
+if ($con = connect_db()) {
+    echo "DB Connect : Success !\n";
+} else {
+    echo "DB Connect : Failure !\n";
+}
+
+if (is_pseudo_exist($con, "root")) {
+    echo "We can read data !";
+} else {
+    echo "We can't read data :(";
 }
